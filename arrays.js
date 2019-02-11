2d99dc0d563c59e991fcc5067c0e946c27f620d5
Mallory Woods <malvwoods@gmail.com>
2019-02-11 22:49:49 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -38,6 +38,6 @@ function destructivelyRemoveElementFromEndOfArray (array) {
 }
 
 function removeElementFromEndOfArray (array) {
-  array.slice(3, array.length - 3)
+  array.slice(-2, array.length - 1)
   return(array)
 }
